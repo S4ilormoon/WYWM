7d92999f04b5355f8f51f4b01e44fe87a6da3622
Tom <80506934+S4ilormoon@users.noreply.github.com>
2021-06-03 10:33:25 +1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+<!-- a webpage about the bitcoin price. -->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>A site about bitcoin</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1>Bitcoin live price</h1>
+        <div>
+            <p>Click the button below to reveal the latest bitcoin price!</p>
+            <select>
+                <option></option>
+            </select>
+            <input type="button">
+        </div>
+        <div>
+            <p source="" alt=""></p>
+        </div>
+    </body>
+</html>
